28f7122184846a0c617f1bf2cbeaf5e8e257bca9
kiraraako <kiraraako@outlook.com>
2023-03-10 15:51:20 +0800

    feat: sequential stack

---
+++ stack/sequential_stack.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <malloc.h>
+
+#define MaxSize 500
+typedef int ElementType;
+
+typedef struct
+{
+    ElementType data[MaxSize]; // 存放栈中的数据元素
+    int top;                   // 栈顶指针
+} SqStack;                     // 顺序栈类型
+
+void InitStack(SqStack *&s)
+{
+    s = (SqStack *)malloc(sizeof(SqStack)); // 分配一个顺序栈空间，首地址放在 s 中
+    s->top = -1;                            // 栈顶指针置为-1
+}
+
+void DestroyStack(SqStack *&s)
+{
+    free(s);
+}
+
+bool StackEmpty(SqStack *&s)
+{
+    return (s->top == -1);
+}
+
+bool Push(SqStack *&s, ElementType &e)
+{
+    if (s->top == MaxSize - 1) // 栈满的情况，栈上溢出
+    {
+        return false;
+    }
+    s->top++;            // 栈顶指针增 1
+    s->data[s->top] = e; // 元素 e 放在栈顶指针处
+    return true;
+}
+
+bool Pop(SqStack *&s, ElementType &e)
+{
+    if (s->top == -1) // 栈满的情况，栈上溢出
+    {
+        return false;
+    }
+    e = s->data[s->top]; // 取栈顶元素
+    s->top--;            // 栈顶指针减一
+    return true;
+}
+
+bool GetTop(SqStack *s, ElementType &e)
+{
+    if (s->top == -1) // 栈为空的情况，即栈下溢出
+    {
+        return false;
+    }
+    e = s->data[s->top]; // 取栈顶元素
+    return true;
+}
+
+bool symmetry(ElementType str[])
+{
+    ElementType e;
+    SqStack *st;
+    InitStack(st);
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        Push(st, str[i]);
+    }
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        Pop(st, e);
+        if (str[i] != e)
+        {
+            DestroyStack(st);
+            return false;
+        }
+    }
+    DestroyStack(st);
+    return true;
+}
+
+int main()
+{
+    return 0;
+}

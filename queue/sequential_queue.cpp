2f3287769e1bd6c996851d92ac999f8fe039cca5
kiraraako <shuoruiako@outlook.com>
2023-03-14 21:36:34 +0800

    feat: sequentail queue

---
+++ queue/sequential_queue.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <malloc.h>
+#include "common.h"
+
+typedef struct
+{
+    ElemType data[MaxSize];
+    int front, rear;
+} SqQueue;
+
+void InitQueue(SqQueue *&q)
+{
+    q = (SqQueue *)malloc(sizeof(SqQueue));
+    q->front = q->rear = -1;
+}
+
+void DestroyQueue(SqQueue *&q)
+{
+    free(q);
+}
+
+bool QueueEmpty(SqQueue *q)
+{
+    return (q->front == q->rear);
+}
+
+bool enQueue(SqQueue *&q, ElemType e)
+{
+    if (q->rear == MaxSize - 1)
+    {
+        return false;
+    }
+    q->rear++;
+    q->data[q->rear] = e;
+    return true;
+}
+
+bool deQueue(SqQueue *&q, ElemType &e)
+{
+    if (q->front == q->rear)
+    {
+        return false;
+    }
+    q->front;
+    e = q->data[q->front];
+    return true;
+}
+
+int main()
+{
+}
